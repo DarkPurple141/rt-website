ba1cca336c1eb952f84b1474f48e659f5c897259
DarkPurple141 <alex.hinds141@gmail.com>
2020-07-23 02:16:56 +0000

    Deploying to gh-pages from  @ 3369ddd6fab409d3ac54250808ae9c91071bf7e4 🚀

---
+++ _next/static/MWjrFmJ1odSEqC3epiPo8/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = (function(a){return {"/":[a],"/[project]":[a],"/about":[a],"/index":[a]}}("static\u002Fchunks\u002F60c79ecb3073baa87f37cc4b947fa854d1c6f891.11c2304b6dd6118b5176.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
