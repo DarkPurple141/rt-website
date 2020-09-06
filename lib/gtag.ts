d0f713a19140412fafaddc210e1dc41e1b16af3d
Alex Hinds <alex.hinds141@gmail.com>
2020-09-06 14:54:46 +1000

    fix: add MT's tag

--- lib/gtag.ts
+++ lib/gtag.ts
@@ -1,5 +1,5 @@
 // https://hoangtrinhj.com/using-google-analytics-with-next-js
-export const GA_TRACKING_ID = 'UA-25942721-5'
+export const GA_TRACKING_ID = 'UA-177304801-1'
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/pages
 export const pageview = (url: string) => {
