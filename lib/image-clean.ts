6825c697a3a491d9bcab2634e4a426e217ff230c
Alex Hinds <alex.hinds141@gmail.com>
2021-05-20 16:10:50 +1000

    chore: don't cut off request too early

--- lib/image-clean.ts
+++ lib/image-clean.ts
@@ -11,24 +11,25 @@ import path from 'path'
  * @returns The root relative path to the final asset
  */
 export async function writeImageToLocal(url: string): Promise<string> {
-  return new Promise(res => {
+  return new Promise((res, rej) => {
     const parsedUrl = u.parse(url)
     const base = path.basename(parsedUrl.pathname!.replace(/\%2B/g, '_'))
     const servedFileName = path.join('/img', base)
     const fileName = path.join(process.cwd(), './public/img', base)
 
-    const request = http.request(url, response => {
+    http.request(url, response => {
       const data = new Stream()
 
       response.on('data', chunk => {
         data.push(chunk)
       })
 
+      response.on('error', rej)
+
       response.on('end', () => {
         fs.writeFileSync(fileName, data.read())
+        res(servedFileName)
       })
     })
-
-    request.end(() => res(servedFileName))
   })
 }
